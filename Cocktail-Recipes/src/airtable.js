401287ada8dd7a4573ffbc89b693350e7d7e7909
Owenlrj98 <owenlrj98@gmail.com>
2024-08-15 00:15:14 +0800

    hide airtable apikey token

--- Cocktail-Recipes/src/airtable.js
+++ Cocktail-Recipes/src/airtable.js
@@ -2,7 +2,7 @@
 const headers = {
   "Content-Type": "application/json",
   Authorization:
-"Bearer patK91PcHhQ6JyNIO.8d4dd38feb7b4aed2d34001682dd1a7628d5651fa441c076d805081351f41b32"
+`Bearer ${import.meta.env.VITE_APIKEY}`
 };
 
 export const addCocktail = async (name, thumb, id) => {
--- Cocktail-Recipes/src/pages/FavouriteCocktailPage.jsx
+++ Cocktail-Recipes/src/pages/FavouriteCocktailPage.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const headers = {
   "Content-Type": "application/json",
   Authorization:
-    "Bearer patK91PcHhQ6JyNIO.8d4dd38feb7b4aed2d34001682dd1a7628d5651fa441c076d805081351f41b32",
+    `Bearer ${import.meta.env.VITE_APIKEY}`,
 };
 
 async function getFavouriteData() {
